8f41d2655b23cf375b31716a3184c3a0c2f53ea3
pzografos <pzograf@hotmail.com>
2016-04-18 01:23:07 +0300

    Corrected run commands in utils

--- tpns-docker-images/util/dockercommands.sh
+++ tpns-docker-images/util/dockercommands.sh
@@ -3,10 +3,10 @@ docker rm $(docker ps -a -q)
 docker rmi `docker images --filter 'dangling=true' -q --no-trunc`
 
 # DATABASE
-docker run -d --name=tpns_db  -e POSTGRES_PASSWORD=123456 -e TPNS_PASS=tpns -p 5432:5432 pzografos/tpns-database
+docker run --net=tpns_nw -itd --name=tpnsdatabase -p 5432:5432 pzografos/tpns-database
 docker build -t pzografos/tpns-database .
 
 # APPLICATION SERVER
-docker run -it --name=tpns_appserver --link tpns_db:postgres -p 127.0.0.1:8080:8080 pzografos/tpns-appserver
+docker run --net=tpns_nw -itd --name=tpnsappserver --link tpnsdatabase:postgres -p 127.0.0.1:8080:8080 pzografos/tpns-appserver
 docker build -t pzografos/tpns-appserver .
 
